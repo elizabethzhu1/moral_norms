c4377fae3cdd36c8a0be63149454c76a77162f67
elizabethzhu1 <elizabethzhuca@gmail.com>
2025-05-07 14:14:17 -0700

    fixes to ethics

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import re
-from datasets import load_dataset
+from datasets import load_dataset, concatenate_datasets
 import random
+import pandas as pd
+import json
 
 SYSTEM_PROMPT = (
     """Reason about the following moral scenario provided by the User. For each scenario, you must provide ALL of the following in order:
@@ -100,12 +102,12 @@ def make_conversation_moral_stories(example):
 
 def make_conversation_scruples(example):
     # Remove unnecessary columns
-    example = {k: v for k, v in example.items() if k in ['action', 'text', 'post_type', 'binared_label']}
+    example = {k: v for k, v in example.items() if k in ['action', 'text', 'post_type', 'binarized_label']}
 
     ground_truth_label = random.choice(['A', 'B'])
 
     # In dataset, 0 is 'Right' and 1 is 'Wrong'
-    ground_truth = example['binared_label']
+    ground_truth = example['binarized_label']
     
     if ground_truth == 'Right':
         not_ground_truth = 'Wrong'
@@ -131,56 +133,197 @@ def make_conversation_scruples(example):
     }
 
 
-def make_conversation_ethics(example):
+def make_conversation_ethics_commonsense(example):
+     # Remove unnecessary columns
+        example = {k: v for k, v in example.items() if k in ['label', 'input']}
+
+        ground_truth_label = random.choice(['A', 'B'])
+
+        # In dataset, 0 is 'Right' and 1 is 'Wrong'
+        if example['label'] == 0:
+            ground_truth = 'Right'
+            not_ground_truth = 'Wrong'
+        else:
+            ground_truth = 'Wrong'
+            not_ground_truth = 'Right'
+        
+        if ground_truth_label == 'A':
+            options = [ground_truth, not_ground_truth]
+        else:
+            options = [not_ground_truth, ground_truth]
+        
+        return {
+            f"prompt": [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": f"""Scenario: {example['input']}.
+                Between the following two judgments A and B, which one reflects this action?
+                A. {options[0]}
+                B. {options[1]}"""},
+            ],
+            "ground_truth": ground_truth_label
+        }
+
+
+def make_conversation_ethics_deontology(example):
+     # Remove unnecessary columns
+        example = {k: v for k, v in example.items() if k in ['label', 'scenario', 'excuse']}
+
+        ground_truth_label = random.choice(['A', 'B'])
+
+        # 1 is 'Right' and 0 is 'Wrong'
+        if example['label'] == 0:
+            ground_truth = 'Inappropriate'  # to describe whether the excuse is justified
+            not_ground_truth = 'Appropriate'  # to describe whether the excuse is justified
+        else:
+            ground_truth = 'Appropriate'  # to describe whether the excuse is justified
+            not_ground_truth = 'Inappropriate'  # to describe whether the excuse is justified
+        
+        if ground_truth_label == 'A':
+            options = [ground_truth, not_ground_truth]
+        else:
+            options = [not_ground_truth, ground_truth]
+
+        return {
+            f"prompt": [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": f"""Scenario: {example['scenario']}.
+                Excuse: {example['excuse']}
+                Is this excuse appropriate? Select A or B.
+                A. {options[0]}
+                B. {options[1]}"""},
+            ],
+            "ground_truth": ground_truth_label
+        }
+
+
+def make_conversation_ethics_justice(example):
+        # Remove unnecessary columns
+        example = {k: v for k, v in example.items() if k in ['label', 'scenario']}
+
+        ground_truth_label = random.choice(['A', 'B'])
+
+        # In dataset, 1 is 'Justified' and 0 is 'Unjustified'
+        if example['label'] == 0:
+            ground_truth = 'Unjustified'
+            not_ground_truth = 'Justified'
+        else:
+            ground_truth = 'Justified'
+            not_ground_truth = 'Unjustified'
+        
+        if ground_truth_label == 'A':
+            options = [ground_truth, not_ground_truth]
+        else:
+            options = [not_ground_truth, ground_truth]
+        
+        return {
+            f"prompt": [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": f"""Statement: {example['scenario']}.
+                Is this statement justified or unjustified? Select A or B.
+                A. {options[0]}
+                B. {options[1]}"""},
+            ],
+            "ground_truth": ground_truth_label
+        }
+
+
+def make_conversation_utilitarianism(example):
     # Remove unnecessary columns
-    example = {k: v for k, v in example.items() if k in ['label', 'input']}
+        example = {k: v for k, v in example.items() if k in ['baseline', 'less_pleasant']}
 
-    ground_truth_label = random.choice(['A', 'B'])
+        baseline = random.choice(['A', 'B'])
 
-    # In dataset, 0 is 'Right' and 1 is 'Wrong'
-    if example['label'] == 0:
-        ground_truth = 'Right'
-        not_ground_truth = 'Wrong'
-    else:
-        ground_truth = 'Wrong'
-        not_ground_truth = 'Right'
-    
-    if ground_truth_label == 'A':
-        options = [ground_truth, not_ground_truth]
-    else:
-        options = [not_ground_truth, ground_truth]
-    
-    return {
-        f"prompt": [
-            {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": f"""Scenario: {example['input']}.
-            Between the following two judgments A and B, which one reflects this action?
-            A. {options[0]}
-            B. {options[1]}"""},
-        ],
-        "ground_truth": ground_truth_label
-    }
+        if baseline == 'A':
+            options = [example['baseline'], example['less_pleasant']]
+        else:
+            options = [example['less_pleasant'], example['baseline']]
+        
+        return {
+            f"prompt": [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": f"""
+                Which of the following scenarios is more pleasant? Select A or B.
+                A. {options[0]}
+                B. {options[1]}"""},
+            ],
+            "ground_truth": baseline
+        }
 
 
-def get_training_dataset(ds_train):
-    # filter out examples where either moral action or immoral action is "not specified"
-    ds_train = ds_train.filter(lambda x: x["moral_action"] != "not specified" and x["immoral_action"] != "not specified")
-
-    train_dataset_ethics = ds_train.map(make_conversation_ethics).select_columns(['prompt', 'ground_truth'])
-    train_dataset_moral = ds_train.map(make_conversation_moral_stories).select_columns(['prompt', 'ground_truth'])
-    train_dataset_scruples = ds_train.map(make_conversation_scruples).select_columns(['prompt', 'ground_truth'])
-
-    train_dataset = train_dataset_ethics + train_dataset_moral + train_dataset_scruples
+def get_training_dataset(ds_train, dataset_name):
+    if dataset_name == "ethics_commonsense":
+        train_dataset = ds_train.map(make_conversation_ethics_commonsense).select_columns(['prompt', 'ground_truth'])
+    elif dataset_name == "ethics_deontology":
+        train_dataset = ds_train.map(make_conversation_ethics_deontology).select_columns(['prompt', 'ground_truth'])
+    elif dataset_name == "ethics_justice":
+        train_dataset = ds_train.map(make_conversation_ethics_justice).select_columns(['prompt', 'ground_truth'])
+    elif dataset_name == "utilitarianism":
+        train_dataset = ds_train.map(make_conversation_utilitarianism).select_columns(['prompt', 'ground_truth'])
+    elif dataset_name == "moral_stories":
+        train_dataset = ds_train.map(make_conversation_moral_stories).select_columns(['prompt', 'ground_truth'])
+    elif dataset_name == "scruples":
+        train_dataset = ds_train.map(make_conversation_scruples).select_columns(['prompt', 'ground_truth'])
+    elif dataset_name == "moral_stories":
+        # filter out examples where either moral action or immoral action is "not specified"
+        ds_train = ds_train.filter(lambda x: x["moral_action"] != "not specified" and x["immoral_action"] != "not specified")
+        train_dataset = ds_train.map(make_conversation_moral_stories).select_columns(['prompt', 'ground_truth'])
 
     return train_dataset
 
 
-def get_eval_dataset(ds_eval):
-    eval_dataset = ds_eval.filter(lambda x: x["moral_action"] != "not specified" and x["immoral_action"] != "not specified")
+def get_eval_dataset(ds_eval, dataset_name):
+
+    # Filter Moral Stories dataset
+    if dataset_name == "moral_stories":
+        ds_eval = ds_eval.filter(lambda x: x["moral_action"] != "not specified" and x["immoral_action"] != "not specified")
+
+    if dataset_name == "ethics":
+        eval_dataset = ds_eval.map(make_conversation_ethics).select_columns(['prompt'])
+    elif dataset_name == "moral_stories":
+        eval_dataset = ds_eval.map(make_conversation_moral_stories).select_columns(['prompt'])
+    elif dataset_name == "scruples":
+        eval_dataset = ds_eval.map(make_conversation_scruples).select_columns(['prompt'])
+
     return eval_dataset
 
 
-if __name__ == "__main__":
-    ds_train = load_dataset("demelin/moral_stories", "full", split='train')
-    train_dataset = get_training_dataset(ds_train)
+def main():
+    ds_train_moral_stories = load_dataset("demelin/moral_stories", "full", split='train')
+    ds_train_scruples = load_dataset("metaeval/scruples", split='train')
+    ds_train_ethics_commensense = load_dataset("hendrycks/ethics", "commonsense", split='train')
+    ds_train_ethics_deontology = load_dataset("hendrycks/ethics", "deontology", split='train')
+    ds_train_ethics_justice = load_dataset("hendrycks/ethics", "justice", split='train')
+    ds_train_utilitarianism = load_dataset("hendrycks/ethics", "utilitarianism", split='train')
+
+    train_dataset_moral_stories = get_training_dataset(ds_train_moral_stories, "moral_stories")
+    train_dataset_ethics_commensense = get_training_dataset(ds_train_ethics_commensense, "ethics_commonsense")
+    train_dataset_ethics_deontology = get_training_dataset(ds_train_ethics_deontology, "ethics_deontology")
+    train_dataset_ethics_justice = get_training_dataset(ds_train_ethics_justice, "ethics_justice")
+    train_dataset_utilitarianism = get_training_dataset(ds_train_utilitarianism, "utilitarianism")
+    train_dataset_scruples = get_training_dataset(ds_train_scruples, "scruples")
+
+    first_rows = {
+        "moral_stories": train_dataset_moral_stories[0],
+        "ethics_commonsense": train_dataset_ethics_commensense[0],
+        "ethics_deontology": train_dataset_ethics_deontology[0], 
+        "ethics_justice": train_dataset_ethics_justice[0],
+        "utilitarianism": train_dataset_utilitarianism[0],
+        "scruples": train_dataset_scruples[0]
+    }
+    
+    with open('first_rows.json', 'w') as f:
+        json.dump(first_rows, f, indent=4)
+
+    train_dataset = concatenate_datasets([
+        train_dataset_moral_stories,
+        train_dataset_ethics_commensense,
+        train_dataset_ethics_deontology,
+        train_dataset_ethics_justice,
+        train_dataset_utilitarianism,
+        train_dataset_scruples
+    ])
+
     print(train_dataset[0])
+
+if __name__ == "__main__":
+    main()
