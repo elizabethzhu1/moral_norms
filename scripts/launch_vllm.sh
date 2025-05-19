ee38d6347320736b31b4b007436272ca6e55dba3
Kanishk Gandhi <kanishk050296@gmail.com>
2025-05-19 14:03:11 -0700

    debug

--- scripts/launch_vllm.sh
+++ scripts/launch_vllm.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 trl vllm-serve --model Qwen/Qwen3-1.7B --tensor_parallel_size 1 --enable_prefix_caching True --gpu_memory_utilization 0.95 --host 0.0.0.0 --port 8001
+CUDA_VISIBLE_DEVICES=0 trl vllm-serve --model Qwen/Qwen3-1.7B-Base --tensor_parallel_size 1 --enable_prefix_caching True --gpu_memory_utilization 0.95 --host 0.0.0.0 --port 8001
