5f70deb9996d7c0fe916f8a309e5940ae66ce825
Kanishk Gandhi <kanishk050296@gmail.com>
2025-05-19 13:29:17 -0700

    debug

--- scripts/launch_vllm.sh
+++ scripts/launch_vllm.sh
@@ -1,1 +1,1 @@
-trl vllm-serve --model Qwen/Qwen2.5-3B --tensor_parallel_size 1 --enable_prefix_caching True --gpu_memory_utilization 0.95 --host 0.0.0.0 --port 8000
+trl vllm-serve --model Qwen/Qwen3-1.7B --tensor_parallel_size 1 --enable_prefix_caching True --gpu_memory_utilization 0.95 --host 0.0.0.0 --port 8000
