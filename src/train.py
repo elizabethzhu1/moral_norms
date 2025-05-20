b58748a8c9ab434af1b639a0422cc3a11054a278
Kanishk Gandhi <kanishk050296@gmail.com>
2025-05-20 15:15:26 -0700

    debug

--- src/train.py
+++ src/train.py
@@ -407,9 +407,9 @@ training_args = GRPOConfig(
     vllm_enable_prefix_caching=True,
     log_completions=True,
     num_completions_to_print=10,
-    # sync_ref_model=True,
-    # ref_model_sync_steps=50,
-    # ref_model_mixup_alpha=0.6
+    sync_ref_model=True,
+    ref_model_sync_steps=50,
+    ref_model_mixup_alpha=1.0
     )
 
 trainer = GRPOTrainer(
